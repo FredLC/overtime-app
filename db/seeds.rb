379d2c8b44bb1ed71d87c4d0712f459195d6ee02
fredlc <fredlefevre@outlook.com>
2020-05-22 09:56:26 -0400

    Implemented the ability to edit post status

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,17 @@
 
 puts "1 user created"
 
+AdminUser.create(
+  			email: "admin@test.com", 
+  			password: "asdfasdf", 
+  			password_confirmation: "asdfasdf",
+  			first_name: "Admin",
+  			last_name: "Mambre"
+  			)
+
+
+puts "1 admin user created"
+
 10.times do |post|
 	Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
