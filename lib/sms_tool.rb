1d891a120221d7ab0e04c736d8d4f135ce248f19
fredlc <fredlefevre@outlook.com>
2020-05-24 14:01:44 -0400

    Integrated live connection with Twilio

--- lib/sms_tool.rb
+++ lib/sms_tool.rb
@@ -1,6 +1,14 @@
 module SmsTool
+	account_sid = Rails.application.credentials.twilio_account_sid
+	auth_token = Rails.application.credentials.twilio_auth_token
+
+	@client = Twilio::REST::Client.new account_sid, auth_token
+
 	def self.send_sms(number:, message:)
-		puts "Sending SMS..."
-		puts "#{message} to #{number}"
+		@client.messages.create(
+	      from: Rails.application.credentials.twilio_phone_number,
+	      to: "+1#{number}",
+	      body: "#{message}"
+	    )
 	end
 end
